5995dec4a8e2d16670ea5767c2d7381913faf5e7
Anton Chepko <choptonys@gmail.com>
2023-03-06 22:08:52 +0100

    js complex dec 2022 homework JS lesson 10  (06.03.2023)

--- 2. JavaScript/hw10 - events/main.js
+++ 2. JavaScript/hw10 - events/main.js
@@ -5,28 +5,49 @@ submit.button.innerText = 'submit info'
 submit.addEventListener('submit',function (e){
     e.preventDefault();
     let div1 = document.createElement('div');
-    div1.innerText = `${submit.name.value} ${submit.surname.value} ${submit.age.value}`
+    div1.innerText = `${this.name.value} ${this.surname.value} ${this.age.value}`
     submit.append(div1);
 });
 // ==========================
 // є сторінка, на якій є блок, я кому знаходиьтся цифра. написати код, який при кожному перезавантажені сторінки буде додавати до неї +1
 
-let number = document.createElement('h3');
-number.innerText = '1';
-document.body.appendChild(number);
-document.addEventListener('load',function (e) {
-    number.innerText += '1'
+// option 1
+let number = localStorage.getItem('h3') || 0;
+let numberHTML = document.createElement('h3');
+numberHTML.innerText = `${+(JSON.parse(number))+1}`
+localStorage.setItem('h3',JSON.stringify(numberHTML.innerText))
+document.body.appendChild(numberHTML);
 
-})
+// option 2
+// if ( localStorage.getItem('h3')){
+//     let numberHTML = document.createElement('h3');
+//     numberHTML.innerText = `${+(JSON.parse(localStorage.getItem('h3')))+1}`
+//     document.body.appendChild(numberHTML);
+//     localStorage.setItem('h3',JSON.stringify(numberHTML.innerText))
+// }else if (!localStorage.getItem('h3')){
+//     localStorage.setItem('h3','0')
+//     localStorage.getItem('h3')
+//     let numberHTML = document.createElement('h3');
+//     numberHTML.innerText = `${+(JSON.parse(localStorage.getItem('h3')))+1}`
+//     document.body.appendChild(numberHTML);
+//     localStorage.setItem('h3',JSON.stringify(numberHTML.innerText))
+// }
+
 // ==========================
 // Є сторінка index.html (назва довільна), при відвідуванні якої в локальне сховще, в масив sessions зберігається інформація про дату та час відвідування сторінки. Є ще сторінка sessions.html (назва довільна), при відвідуванні якої потрібно відмалювати всю інформацію про відвідування сторінки index.html. Інфу НЕ виводити в консоль, а побудувати дом структуру під кожну сессію
+let sessionsDate = JSON.parse(localStorage.getItem('sessions')) || [];
+let date = new Date();
+sessionsDate.push(date);
+localStorage.setItem('sessions',JSON.stringify(sessionsDate));
+
+
 // =========================
 //     зробити масив на 100 об'єктів та дві кнопки prev next
 // при завантажені сторінки з'являються перші 10 об'єктів.
 //     При натисканні next виводяться настпні 10 об'єктів
 // При натисканні prev виводяться попередні 10 об'єктів
-//
-//
+
+
 // *** Створити 3 інпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
 //     При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
 // (Додатковачастина для завдання)sa
